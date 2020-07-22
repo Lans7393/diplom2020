dac805980f85a186b9fbbdc0d5c2c14bc54cd026
Dmitrii <rdaprog@gmail.com>
2020-07-22 17:10:50 +0300

    рестарт 22.07

--- diplom/apps/attemption1/admin.py
+++ diplom/apps/attemption1/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Activity, Product, Company
+from .models import Okved, Okpd, Company
+from django.utils.safestring import mark_safe
 
 from django.utils.html import format_html
 
@@ -11,14 +12,14 @@ from django.utils.html import format_html
 #     extra = 1
 
 
-@admin.register(Activity)
+@admin.register(Okved)
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'okved2')
     search_fields = ('name', 'okved2',)
     ordering = ('id',)
 
 
-@admin.register(Product)
+@admin.register(Okpd)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'okpd2',)
     # list_filter = ('is_active', 'type',)
@@ -28,27 +29,28 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'inn', 'address', 'get_activities', 'get_site_link', 'get_list_org_link')
-    search_fields = ('name', 'postal_code', 'address', 'site')
-    filter_horizontal = ('activities',)
+    list_display = ('id', 'name', 'main_okved', 'get_sup_okveds', 'get_list_org_link')
+    search_fields = ('name', 'inn', 'list_org_link')
+    filter_horizontal = ('sup_okveds',)
+    raw_id_fields = ('main_okved',)
 
     # autocomplete_fields = ('activities',)
     # date_hierarchy = 'created'
     # prepopulated_fields = {'slug': ('title',)} # поле slug не требуется вводить, оно заполняется автоматически значением поля title
-    # raw_id_fields = ('author',) # foreign_key при редактировании объекта будет поле для поиска а не дропдаун лист
     # inlines = [CompanyActivitiesInline]
 
 
-    def get_activities(self, obj):
-        activities_str = ("\n".join([a.okved2 + ', ' for a in obj.activities.all()]))
-        return activities_str[:-2]
+    def get_sup_okveds(self, obj):
+        okveds_str = ("<br>".join([a.okved2 + ' ' + a.name for a in obj.sup_okveds.all()]))
+        return mark_safe(okveds_str[:-2])
 
-    get_activities.short_description = 'Виды деятельности'
 
-    def get_site_link(self, obj):
-        return format_html("<a href='{url}'>{url}</a>", url=obj.site)
+    get_sup_okveds.short_description = 'Виды деятельности'
 
-    get_site_link.short_description = 'Сайт компании'
+    # def get_site_link(self, obj):
+    #     return format_html("<a href='{url}'>{url}</a>", url=obj.site)
+    #
+    # get_site_link.short_description = 'Сайт компании'
 
     def get_list_org_link(self, obj):
         return format_html("<a href='{url}'>{url}</a>", url=obj.list_org_link)
--- diplom/apps/attemption1/models.py
+++ diplom/apps/attemption1/models.py
@@ -1,22 +1,23 @@
 from django.db import models
+from phone_field import PhoneField
 
 
-class Activity(models.Model):
-    okved2 = models.CharField('ОКВЭД2', max_length=12, blank=True, null=True)
+class Okved(models.Model):
+    okved = models.CharField('ОКВЭД', max_length=12, blank=True, null=True)
+    okved2 = models.CharField('ОКВЭД2', max_length=12, blank=True, null=True, unique=True)
     name = models.CharField('Наименование', max_length=512)
 
     def __str__(self):
         return self.okved2 + ' ' + self.name
 
-
     class Meta:
         managed = True
-        db_table = 'activity'
+        db_table = 'okved'
         verbose_name = 'Вид деятельности'
         verbose_name_plural = 'Виды деятельности'
 
 
-class Product(models.Model):
+class Okpd(models.Model):
     okpd2 = models.CharField('ОКПД2', unique=True, max_length=12, blank=True, null=True)
     name = models.CharField('Наименование', max_length=512, blank=True, null=True)
 
@@ -24,25 +25,99 @@ class Product(models.Model):
         managed = True
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-        db_table = 'product'
+        db_table = 'okpd'
 
 
 class Company(models.Model):
     name = models.CharField('Название', max_length=256, blank=True, null=True)
     short_name = models.CharField('Короткое название', max_length=64, blank=True, null=True)
+
     inn = models.CharField('ИНН', max_length=12, blank=True, null=True)
-    address = models.CharField('Адрес', max_length=512, blank=True, null=True)
-    postal_code = models.CharField('Почтовый индех', max_length=64, blank=True, null=True)
-    activities = models.ManyToManyField(Activity, related_name='companies', blank=True)
+    kpp = models.CharField('КПП', max_length=32, blank=True, null=True)
+    okpo = models.CharField('ОКПО', max_length=32, blank=True, null=True)
+    ogrn = models.CharField('ОГРН', max_length=32, blank=True, null=True)
+    okfs = models.CharField('ОКФС', max_length=32, blank=True, null=True)
+    okopf = models.CharField('ОКОПФ', max_length=32, blank=True, null=True)
+    oktmo = models.CharField('ОКТМО', max_length=32, blank=True, null=True)
+    okato = models.CharField('ОКАТО', max_length=32, blank=True, null=True)
+    okogu = models.CharField('ОКОГУ', max_length=32, blank=True, null=True)
+
+    main_okved = models.ForeignKey(Okved, on_delete=models.DO_NOTHING, null=True, blank=True)
+    sup_okveds = models.ManyToManyField(Okved, related_name='companies', blank=True)
 
     list_org_link = models.URLField('Ссылка на list-org.com', null=True, blank=True)
-    site = models.URLField('Сайт компании', null=True, blank=True)
 
     def __str__(self):
-        return self.short_name
+        return self.short_name if self.short_name else self.name
 
     class Meta:
         managed = True
         db_table = 'company'
         verbose_name = 'Компания'
         verbose_name_plural = 'Компании'
+
+
+class Address(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING, null=True)
+
+    postal_code = models.CharField('Почтовый индех', max_length=64, blank=True, null=True)
+    federal_subject = models.CharField('Субъект федерации', max_length=128, blank=True, null=True)
+    locality = models.CharField('Населенный пункт', max_length=128, blank=True, null=True)
+    street = models.CharField('Улица', max_length=256, blank=True, null=True)
+    remain = models.CharField('Номер дома, квартиры, строения и т.п.', max_length=256, blank=True, null=True)
+    gps_coordinates = models.CharField('GPS координаты', max_length=32, blank=True, null=True)
+    comment = models.CharField('Комментарий', max_length=256, blank=True, null=True)
+    is_legal = models.BooleanField('Юридический адрес', default=False)
+
+    class Meta:
+        managed = True
+        db_table = 'address'
+        verbose_name = 'Адрес'
+        verbose_name_plural = 'Адреса'
+
+
+class Phone(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING, null=True)
+    phone = PhoneField('Телефон', blank=True, null=False, help_text='Номер телефона')
+    comment = models.CharField('Комментарий', max_length=256, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'phone'
+        verbose_name = 'Телефон'
+        verbose_name_plural = 'Телефоны'
+
+
+class Fax(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING, null=True)
+    fax = PhoneField('Факс', blank=True, null=False, help_text='Номер факса')
+    comment = models.CharField('Комментарий', max_length=256, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'fax'
+        verbose_name = 'Факс'
+        verbose_name_plural = 'Факсы'
+
+
+class Email(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING, null=True)
+    email = models.EmailField('E-mail', blank=True, null=False, help_text='E-mail')
+    comment = models.CharField('Комментарий', max_length=256, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'email'
+        verbose_name = 'E-mail'
+
+
+class Site(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING, null=True)
+    site = models.URLField('Сайт', null=False, blank=True, help_text='Сайт')
+    comment = models.CharField('Комментарий', max_length=256, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'site'
+        verbose_name = 'Сайт'
+        verbose_name_plural = 'Сайты'
